757d0b3974a29ca7251e601f20b013c98a159705
AleenaNadeem101 <aleenanadeem622@gmail.com>
2026-06-26 07:52:26 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello Aleena CI/CD Pipeline") 
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end("Hello Aleena CI/CD Pipeline - v1\n");
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
